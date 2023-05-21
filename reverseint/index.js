2b70075d667a754c388c4ada775a3ae61a0dc5ac
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-21 23:24:51 +0100

    Math.sign

--- reverseint/index.js
+++ reverseint/index.js
@@ -11,10 +11,12 @@
 function reverseInt(n) {
   const reversed = n.toString().split("").reverse().join("");
 
-  if(n < 0 ) {
-      return parseInt(reversed) * -1;
-  }
-  return parseInt(reversed);
+//   if(n < 0 ) {
+//       return parseInt(reversed) * -1;
+//   }
+//or using Math.sign
+
+  return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
