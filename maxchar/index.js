44aa94a80739741127cfffaba5f4308d3131c753
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-22 23:21:17 +0100

    logical way

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
 
+}
+/// Logic
+const string = "Hello There!"
+const chars = {};
+
+for (let char of string) {
+    if (!chars[char]) {
+        chars[char] = 1;
+    } else {
+        chars[char] ++;
+    }
+}
+
+chars;
 module.exports = maxChar;
