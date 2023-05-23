bb73c972159ebec41b1672de17fad3a865152f91
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-23 23:15:59 +0100

    Chunk first solution

--- chunk/index.js
+++ chunk/index.js
@@ -9,19 +9,19 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const chunked = [];
+  const chunked = [];
 
-    for (let element of array) {
-        const last = chunked[chunked.length -1];
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
 
-        if(!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
     }
+  }
 
-    return chunked;
+  return chunked;
 }
 
 module.exports = chunk;
