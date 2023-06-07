d190f0b8965442882077ec1c75f76f574d691a90
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-07 23:41:10 +0100

    pyramid 2

--- pyramid/index.js
+++ pyramid/index.js
@@ -15,16 +15,20 @@
 //       '#####'
 
 function pyramid(n) {
-    const midpoint = Math.floor((2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-      let level = '';
-      for(let col = 0; col < 2 * n -1; col++) {
-         if(midpoint - row <= column && midpoint + row >= col) {
-             level += '#';
-         } else {
-             level += ' ';
-         }
-  }
-}
+
 }
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (midpoint - row <= column && midpoint + row >= col) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//   }
+// }
