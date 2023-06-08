308d436d08449570529c843a840540c9b4e6f618
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-08 22:37:58 +0100

    Explanations

--- chunk/index.js
+++ chunk/index.js
@@ -19,6 +19,33 @@ function chunk(array, size) {
 
  return chunked;
 }
+
+//A function called chunk that takes an array array and a size size as input and splits the array into smaller chunks of the specified size. 
+//const chunked = [];: 
+//It initializes an empty array called chunked. 
+//This array will store the chunks of the input array.
+
+//let i = 0;: 
+//It initializes a variable i to 0. 
+//This variable will keep track of the current position while iterating over the input array.
+
+//while (i < array.length) { ... }: 
+//It starts a loop that continues until the value of i is less than the length of the input array. 
+//This loop will iterate over the elements of the array.
+
+//chunked.push(array.slice(i, i + size));: 
+//It uses the slice method to extract a portion of the input array starting from the index i and ending at i + size. 
+//The extracted portion represents a chunk of the specified size. 
+//This chunk is then added to the chunked array using the push method.
+
+//i += size;: It increments the value of i by the specified size, moving the current position to the next chunk in the array.
+
+//return chunked;: 
+//After the loop completes, the function returns the chunked array containing all the chunks of the input array.
+
+//The chunk function divides the input array into smaller chunks of the specified size and returns an array of arrays, where each inner array represents a chunk.
+
+
 //First way
 // function chunk(array, size) {
 //   const chunked = [];
