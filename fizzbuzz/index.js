a2e79d4b31645e1c3aec697a3fe640a30be96fbd
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-08 22:42:52 +0100

    Explanations

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -28,4 +28,32 @@ function fizzBuzz(n) {
   }
 }
 
+//A function called fizzBuzz that takes a number n as input and prints a specific string or number based on certain conditions. 
+
+//for (let i = 1; i <= n; i++) { ... }: 
+//It starts a loop that iterates from 1 to n, where n is the input number. 
+//The loop variable i represents the current number being evaluated.
+
+//if (i % 3 === 0 && i % 5 === 0) { ... }: 
+//It checks if the current number i is divisible by both 3 and 5. 
+//If true, it prints the string "fizzbuzz" using the console.log statement.
+
+//else if (i % 3 === 0) { ... }: 
+//If the previous condition is false, it checks if the current number i is divisible by 3. 
+//If true, it prints the string "fizz" using the console.log statement.
+
+//else if (i % 5 === 0) { ... }:
+// If the previous conditions are false, it checks if the current number i is divisible by 5. 
+//If true, it prints the string "buzz" using the console.log statement.
+
+//else { ... }: 
+//If none of the previous conditions are true, it executes the else block and prints the current number i using the console.log statement.
+
+//The FizzBuzz problem requires printing numbers from 1 to n, 
+//but for multiples of 3, "fizz" should be printed instead, 
+//for multiples of 5, "buzz" should be printed, and for numbers 
+//that are multiples of both 3 and 5, "fizzbuzz" should be printed.
+//The fizzBuzz function accomplishes this by iterating over the numbers 
+//from 1 to n and applying the necessary conditions to determine what should be printed. 
+//It uses the modulus operator % to check for divisibility by 3 and 5.
 module.exports = fizzBuzz;
