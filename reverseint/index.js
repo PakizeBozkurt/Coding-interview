903c0fe6a312118cc10708e11265806531b12c5e
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-09 23:11:36 +0100

    Explanations

--- reverseint/index.js
+++ reverseint/index.js
@@ -19,4 +19,23 @@ function reverseInt(n) {
   return parseInt(reversed) * Math.sign(n);
 }
 
+
+//A function called reverseInt that takes an integer n as input and returns the reverse of that integer. 
+
+//The function first converts the input integer n to a string using the toString() method.
+
+//It then splits the string into an array of individual characters using the split("") method.
+
+//The reverse() method is called on the resulting array to reverse the order of the characters.
+
+//The join("") method is used to join the reversed array of characters back into a string.
+
+//The reversed string is then parsed into an integer using the parseInt() function.
+
+//The code multiplies the parsed reversed integer by the sign of the original integer n using Math.sign(n). 
+//This step ensures that the reversed integer retains the original sign of n.
+
+//Finally, the reversed and signed integer is returned as the result of the reverseInt function.
+
+
 module.exports = reverseInt;
