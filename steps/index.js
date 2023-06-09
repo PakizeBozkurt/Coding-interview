0f13afd62cdb5e370e98dcc8d23ea0e602ec2de9
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-09 23:23:14 +0100

    Explanations

--- steps/index.js
+++ steps/index.js
@@ -32,6 +32,29 @@ function steps(n) {
   }
 }
 
+//A function called steps that takes a number n as input and prints a staircase pattern with n steps. 
+
+//The function uses a for loop to iterate over each row of the staircase.
+// The loop variable row starts from 0 and continues as long as row is less than n.
+
+//Inside the outer for loop, a variable called stair is defined as an empty string. 
+//This variable will hold the characters for each row of the staircase.
+
+//There is an inner for loop that iterates over each column within a row. 
+//The loop variable colm starts from 0 and continues as long as colm is less than n.
+
+//Inside the inner for loop, an if statement is used to determine whether to add a # character or a space character to the stair string.
+// If colm is less than or equal to row, it means we are on or before the diagonal of the staircase, so a # is added to the stair string. Otherwise, a space is added.
+
+//After the inner for loop finishes iterating over the columns of the current row, the stair string contains the characters for that row.
+
+//The console.log() function is called to print the stair string, which represents the current row of the staircase.
+
+//The outer for loop continues to the next iteration, incrementing row by 1, and the process is repeated for the remaining rows.
+
+//Once the outer for loop finishes iterating over all the rows, the function completes execution.
+
+
 module.exports = steps;
 
 //function steps(n) {
